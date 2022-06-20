c11a62f64eca1082e9dd7ac98b0c439647ff400a
Vlad Polyakov <vladpolyakov@microsoft.com>
2022-06-20 18:39:25 +0000

    remove build and update .gitignore

--- src/build/CMakeFiles/eakv.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "e_akv.pdb"
-  "e_akv.so"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang C)
-  include(CMakeFiles/eakv.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
