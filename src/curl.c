a8f3de9eded066db869d523f475e893043fa3d52
xmams <122564616+xmams@users.noreply.github.com>
2023-04-17 17:42:21 -0700

    Update curl.c
    correction

--- src/curl.c
+++ src/curl.c
@@ -79,15 +79,12 @@ int GetAccessTokenFromIMDS(const char *type, MemoryStruct *accessToken)
   if (azureCliAccessTokenSize != 0)
   {
     Log(LogLevel_Info, "Environment variable AZURE_CLI_ACCESS_TOKEN defined [%zu]\n", azureCliAccessTokenSize);
-    accessToken->memory  = (char *)malloc(azureCliAccessTokenSize * sizeof(char) + 1);
+    accessToken->memory  = (char *)malloc(azureCliAccessTokenSize * sizeof(char));
     if (!accessToken->memory)
     {
       Log(LogLevel_Error, "Environment variable AZURE_CLI_ACCESS_TOKEN defined, but failed to allocate memory for accessToken->memory!\n");
       return 0;
     }
-    
-    memcpy(accessToken->memory, azureCliToken, azureCliAccessTokenSize);
-    accessToken->memory[azureCliAccessTokenSize] = '\0';
 
     getenv_s(&azureCliAccessTokenSize, accessToken->memory, azureCliAccessTokenSize, "AZURE_CLI_ACCESS_TOKEN");
     accessToken->size = azureCliAccessTokenSize;
@@ -100,7 +97,7 @@ int GetAccessTokenFromIMDS(const char *type, MemoryStruct *accessToken)
   {
     azureCliAccessTokenSize = strlen(azureCliToken);
     Log(LogLevel_Info, "Environment variable AZURE_CLI_ACCESS_TOKEN defined [%zu]\n", azureCliAccessTokenSize);
-    accessToken->memory  = (char *)malloc(azureCliAccessTokenSize * sizeof(char)  );
+    accessToken->memory  = (char *)malloc(azureCliAccessTokenSize * sizeof(char) + 1);
     if (!accessToken->memory)
     {
       Log(LogLevel_Error, "Environment variable AZURE_CLI_ACCESS_TOKEN defined, but failed to allocate memory for accessToken->memory!\n");
@@ -108,7 +105,8 @@ int GetAccessTokenFromIMDS(const char *type, MemoryStruct *accessToken)
     }
 
     memcpy(accessToken->memory, azureCliToken, azureCliAccessTokenSize);
-    accessToken->size = azureCliAccessTokenSize;
+    accessToken->memory[azureCliAccessTokenSize] = '\0';
+    accessToken->size = azureCliAccessTokenSize + 1;
     return 1;
   }
 #endif
